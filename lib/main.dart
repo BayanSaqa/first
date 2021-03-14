ee84b4697a14876c585e697639e6dd51404fe639
BayanSaqa <benocity1998@hotmail.com>
2021-03-14 17:35:34 +0200

    yasmin

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home : Scaffold(
+         appBar: AppBar(
+           title: Text("APP BAR"),
+         ),
+        body:   Text("body BAR"),
+
+      )
+
+    );
+    throw UnimplementedError();
+  }
+
+
+}
