931740ebf1b770278774734feb3838a120766367
BayanSaqa <benocity1998@hotmail.com>
2021-03-16 22:03:12 +0200

    This is First Commit

---
+++ lib/Intro-Screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+ class IntroScreen extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       backgroundColor: Colors.lightBlue,
+       body: Center(
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: <Widget>[
+               SizedBox(),
+               Image.asset('assets/images/market.png'),
+               SizedBox(
+                 height: 60,
+                 width: double.infinity,
+                 child: RaisedButton(
+                   color: Colors.white,
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(25),
+                     ),
+                     onPressed:() {
+                       Navigator.of(context).pushNamed('logIn');
+                     },
+                   child: Text ('Get Started',
+                     style: TextStyle(
+                       color: Colors.lightBlue,
+                       fontSize: 18
+                     ),
+
+                   ),
+
+                 ),
+               ),
+
+             ],
+           ),
+         ),
+       )
+
+     );
+   }
+ }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app111/Intro-Screen.dart';
+
+import 'Auth-Screen.dart';
 
 void main(){
   
@@ -10,13 +13,36 @@ class Myapp extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home : Scaffold(
-         appBar: AppBar(
-           title: Text("APP BAR"),
-         ),
-        body:   Text("body BAR"),
 
-      )
+      home: IntroScreen(),
+routes: {
+        'logIn': (context) {
+          AuthScreen();
+        }
+
+
+},
+//      home : Scaffold(
+//         appBar: AppBar(
+//           title: Text("APP BAR"),
+//         ),
+//        body:   Container(
+//            alignment: Alignment.center,
+//          child: Column(
+//
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children :[
+//                    Text("APP BAR"),
+//                    Text("APP BAR"),
+//                    Text("APP BAR"),
+//                  ]
+//          )
+//
+//
+//
+//        )
+//
+//      )
 
     );
     throw UnimplementedError();
